9bf06add6a2994e11db0e9ec746a31a3270d847b
Polarthebear <thatotae325@gmail.com>
2024-01-21 08:39:08 -0800

    Opcode that does nothing.

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop_ - Main entry point
+ * Description: Opcode that does nothing
+ * @head: head stack
+ * @count: line number
+ */
+void nop_(stack_t **head, unsigned int count)
+{
+	(void) head;
+	(void) counter;
+}
